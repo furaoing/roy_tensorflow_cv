796139b337a648e492b7ed4f5b76c42c5d2e6c1a
Roy_ <furaoing@hotmail.com>
2015-12-28 10:13:42 +0800

    Update classify_image_roy_variant_web.py

--- tornado-upload-master/classify_image_roy_variant_web.py
+++ tornado-upload-master/classify_image_roy_variant_web.py
@@ -43,140 +43,138 @@ import tensorflow.python.platform
 from six.moves import urllib
 import numpy as np
 import tensorflow as tf
+import os
+import sys
 
 # pylint: enable=unused-import,g-bad-import-order
 
 from tensorflow.python.platform import gfile
-    
 
-def predict_image(image, config):
 
-    class NodeLookup(object):
-        """Converts integer node ID's to human readable labels."""
+class NodeLookup(object):
+    """Converts integer node ID's to human readable labels."""
 
-        def __init__(self, config, label_lookup_path=None, uid_lookup_path=None):
-            if not label_lookup_path:
-                label_lookup_path = os.path.join(config.model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
-            if not uid_lookup_path:
-                uid_lookup_path = os.path.join(config.model_dir, 'imagenet_synset_to_human_label_map.txt')
-                self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
-    
-        def load(self, label_lookup_path, uid_lookup_path):
-            """Loads a human readable English name for each softmax node.
-    
-            Args:
-              label_lookup_path: string UID to integer node ID.
-              uid_lookup_path: string UID to human-readable string.
+    def __init__(self, config, label_lookup_path=None, uid_lookup_path=None):
+        if not label_lookup_path:
+            label_lookup_path = os.path.join(config.model_dir, 'imagenet_2012_challenge_label_map_proto.pbtxt')
+        if not uid_lookup_path:
+            uid_lookup_path = os.path.join(config.model_dir, 'imagenet_synset_to_human_label_map.txt')
+            self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
 
-            Returns:
-              dict from integer node ID to human-readable string.
-            """
-            if not gfile.Exists(uid_lookup_path):
-                tf.logging.fatal('File does not exist %s', uid_lookup_path)
-            if not gfile.Exists(label_lookup_path):
-                tf.logging.fatal('File does not exist %s', label_lookup_path)
-    
-                # Loads mapping from string UID to human-readable string
-            proto_as_ascii_lines = gfile.GFile(uid_lookup_path).readlines()
-            uid_to_human = {}
-            p = re.compile(r'[n\d]*[ \S,]*')
-            for line in proto_as_ascii_lines:
-                parsed_items = p.findall(line)
-                uid = parsed_items[0]
-                human_string = parsed_items[2]
-                uid_to_human[uid] = human_string
-
-            # Loads mapping from string UID to integer node ID.
-            node_id_to_uid = {}
-            proto_as_ascii = gfile.GFile(label_lookup_path).readlines()
-            for line in proto_as_ascii:
-                if line.startswith('  target_class:'):
-                    target_class = int(line.split(': ')[1])
-                if line.startswith('  target_class_string:'):
-                    target_class_string = line.split(': ')[1]
-                    node_id_to_uid[target_class] = target_class_string[1:-2]
-
-            # Loads the final mapping of integer node ID to human-readable string
-            node_id_to_name = {}
-            for key, val in node_id_to_uid.items():
-                if val not in uid_to_human:
-                    tf.logging.fatal('Failed to locate: %s', val)
-                name = uid_to_human[val]
-                node_id_to_name[key] = name
-
-            return node_id_to_name
-
-        def id_to_string(self, node_id):
-            if node_id not in self.node_lookup:
-                return ''
-            return self.node_lookup[node_id]
-
-    def create_graph(config):
-        """"Creates a graph from saved GraphDef file and returns a saver."""
-        # Creates graph from saved graph_def.pb.
-        with gfile.FastGFile(os.path.join(config.model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
-            graph_def = tf.GraphDef()
-            graph_def.ParseFromString(f.read())
-            _ = tf.import_graph_def(graph_def, name='')
-
-    def run_inference_on_image(image, config):
-        result_str = ""
-        """Runs inference on an image.
+    def load(self, label_lookup_path, uid_lookup_path):
+        """Loads a human readable English name for each softmax node.
 
         Args:
-        image: Image file name.
+          label_lookup_path: string UID to integer node ID.
+          uid_lookup_path: string UID to human-readable string.
 
         Returns:
-        Nothing
+          dict from integer node ID to human-readable string.
         """
-        if not gfile.Exists(image):
-            tf.logging.fatal('File does not exist %s', image)
-        image_data = gfile.FastGFile(image, 'rb').read()
-    
-        # Creates graph from saved GraphDef.
-        create_graph(config)
-    
-        with tf.Session() as sess:
-            # Some useful tensors:
-            # 'softmax:0': A tensor containing the normalized prediction across
-            #   1000 labels.
-            # 'pool_3:0': A tensor containing the next-to-last layer containing 2048
-            #   float description of the image.
-            # 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
-            #   encoding of the image.
-            # Runs the softmax tensor by feeding the image_data as input to the graph.
-            softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
-            predictions = sess.run(softmax_tensor,
-                                   {'DecodeJpeg/contents:0': image_data})
-            predictions = np.squeeze(predictions)
+        if not gfile.Exists(uid_lookup_path):
+            tf.logging.fatal('File does not exist %s', uid_lookup_path)
+        if not gfile.Exists(label_lookup_path):
+            tf.logging.fatal('File does not exist %s', label_lookup_path)
 
-            # Creates node ID --> English string lookup.
-            node_lookup = NodeLookup(config)
+            # Loads mapping from string UID to human-readable string
+        proto_as_ascii_lines = gfile.GFile(uid_lookup_path).readlines()
+        uid_to_human = {}
+        p = re.compile(r'[n\d]*[ \S,]*')
+        for line in proto_as_ascii_lines:
+            parsed_items = p.findall(line)
+            uid = parsed_items[0]
+            human_string = parsed_items[2]
+            uid_to_human[uid] = human_string
 
-            top_k = predictions.argsort()[-config.num_top_predictions:][::-1]
-            for node_id in top_k:
-                human_string = node_lookup.id_to_string(node_id)
-                score = predictions[node_id]
-                print('%s (score = %.5f)' % (human_string, score))
+        # Loads mapping from string UID to integer node ID.
+        node_id_to_uid = {}
+        proto_as_ascii = gfile.GFile(label_lookup_path).readlines()
+        for line in proto_as_ascii:
+            if line.startswith('  target_class:'):
+                target_class = int(line.split(': ')[1])
+            if line.startswith('  target_class_string:'):
+                target_class_string = line.split(': ')[1]
+                node_id_to_uid[target_class] = target_class_string[1:-2]
 
-                result_str += '%s (score = %.5f)' % (human_string, score) + r"|"
-        return result_str
+        # Loads the final mapping of integer node ID to human-readable string
+        node_id_to_name = {}
+        for key, val in node_id_to_uid.items():
+            if val not in uid_to_human:
+                tf.logging.fatal('Failed to locate: %s', val)
+            name = uid_to_human[val]
+            node_id_to_name[key] = name
 
-    return run_inference_on_image(image, config)
+        return node_id_to_name
+
+    def id_to_string(self, node_id):
+        if node_id not in self.node_lookup:
+            return ''
+        return self.node_lookup[node_id]
+
+def create_graph(config):
+    """"Creates a graph from saved GraphDef file and returns a saver."""
+    # Creates graph from saved graph_def.pb.
+    with gfile.FastGFile(os.path.join(config.model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+def run_inference_on_image(image, node_lookup, config):
+    result_str = ""
+    """Runs inference on an image.
+
+    Args:
+    image: Image file name.
+
+    Returns:
+    Nothing
+    """
+    if not gfile.Exists(image):
+        tf.logging.fatal('File does not exist %s', image)
+    image_data = gfile.FastGFile(image, 'rb').read()
+
+    with tf.Session() as sess:
+        # Some useful tensors:
+        # 'softmax:0': A tensor containing the normalized prediction across
+        #   1000 labels.
+        # 'pool_3:0': A tensor containing the next-to-last layer containing 2048
+        #   float description of the image.
+        # 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
+        #   encoding of the image.
+        # Runs the softmax tensor by feeding the image_data as input to the graph.
+        softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
+        predictions = sess.run(softmax_tensor,
+                               {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+
+        top_k = predictions.argsort()[-config.num_top_predictions:][::-1]
+        for node_id in top_k:
+            human_string = node_lookup.id_to_string(node_id)
+            score = predictions[node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+
+            result_str += '%s (score = %.5f)' % (human_string, score) + r"|"
+    return result_str
 
 
 class roy_config:
-        base_path = sys.path[0]
-        model_dirname = 'imagenet'
-        model_dir = os.path.join(base_path, model_dirname)
-        num_top_predictions = 5
-        DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+    base_path = sys.path[0]
+    relative_path = 'imagenet'
+    model_dir = os.path.join(base_path, relative_path)
+    num_top_predictions = 5
+    DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+
 
 if __name__ == '__main__':
     image_path = "/home/furaoing/roy_tensorflow_cv/tornado-upload-master/uploads/m155jc.jpg"
-    image_path2 = "/home/furaoing/images_test/test1.jpg"
-    
-    predict_image(image_path, roy_config)
+    image_path2 = "/home/furaoing/images_test/test33.jpg"
+
+    config = roy_config
+    node_lookup = NodeLookup(config)
+    # Creates graph from saved GraphDef.
+    create_graph(config)
+    run_inference_on_image(image_path, node_lookup, config)
+    run_inference_on_image(image_path2, node_lookup, config)
 
     """
     time.sleep(3)
