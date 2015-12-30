116ed92f91e7b171c8e451b7bbb6f251ea3b41ba
furaoing <furaoing@hotmail.com>
2015-12-30 13:04:46 +0800

    test git

--- tornado-upload-master/upload.py
+++ tornado-upload-master/upload.py
@@ -15,10 +15,7 @@ import time
 
 import sys
 
-define("port", default=8080, help="run on the given port", type=int)
-
-def test:
-    pass
+define("port", default=80, help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
     def __init__(self):
